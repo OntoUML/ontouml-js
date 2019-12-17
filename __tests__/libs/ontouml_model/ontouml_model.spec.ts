0efb516f4ebec1eb332651f859ddc2b4e2f83a7b
Claudenir M. Fonseca <claudenirmf@gmail.com>
2019-12-17 15:12:47 +0100

    Using local dependencies
    Replacing dependencies of 'ontouml-example-models' for local dependencies for this moment.

--- __tests__/libs/ontouml_model/ontouml_model.spec.ts
+++ __tests__/libs/ontouml_model/ontouml_model.spec.ts
@@ -1,4 +1,6 @@
-import { modelInvalidExample1 } from 'ontouml-example-models';
+// import { modelInvalidExample1 } from 'ontouml-example-models';
+// TODO Replace local dependencies on example models for dependencies to 'ontouml-example-models' project
+import { modelInvalidExample1 } from '@test-models/invalids';
 import OntoUMLModel from '@libs/ontouml_model';
 
 describe('OntoUML Model', () => {
