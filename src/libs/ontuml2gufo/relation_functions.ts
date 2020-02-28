6a266c7f331e1bb2980ca23556f64880d54c49ca
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2020-02-28 12:22:37 -0300

    Add transformation for cardinalities

--- src/libs/ontuml2gufo/relation_functions.ts
+++ src/libs/ontuml2gufo/relation_functions.ts
@@ -1,4 +1,4 @@
-import { N3Writer, Quad } from 'n3';
+import { N3Writer, Quad, BlankNode, Literal } from 'n3';
 import { IRelation } from '@types';
 import { RelationStereotype } from '@constants/.';
 import {
@@ -68,10 +68,16 @@ export async function transformRelationsByStereotype(
     ) {
       // Get domain and range quads from relation
       const domainRangeQuads = transformRelationDomainAndRange(relation);
+      // Get cardinalities quads from relation
+      const cardinalityQuads = transformRelationCardinalities(writer, relation);
       // Get stereotype quads from relation
       const stereotypeQuads = transformStereotypeFunction[stereotype](relation);
 
-      await writer.addQuads([...domainRangeQuads, ...stereotypeQuads]);
+      await writer.addQuads([
+        ...domainRangeQuads,
+        ...cardinalityQuads,
+        ...stereotypeQuads,
+      ]);
 
       // add label
       if (name) {
@@ -114,13 +120,241 @@ export function transformRelationDomainAndRange(relation: IRelation): Quad[] {
 /**
  * Transform relation cardinalities to gUFO
  */
-// export async function transformRelationCardinalities(
-//   relation: IRelation,
-// ): Promise<Quad[]> {
-//   const { properties } = relation;
+export function transformRelationCardinalities(
+  writer: N3Writer,
+  relation: IRelation,
+): Quad[] {
+  const { properties } = relation;
 
-//   const domainCardinality = properties[0].cardinality;
-//   const rangeCardinality = properties[1].cardinality;
+  const domain = properties[0];
+  const range = properties[1];
 
-//   return [];
-// }
+  const domainCardinality = domain.cardinality;
+  const rangeCardinality = range.cardinality;
+
+  if (domainCardinality && rangeCardinality) {
+    return [
+      ...transformRelationCardinality({
+        writer,
+        relation,
+        cardinality: domainCardinality,
+        isDomain: true,
+      }),
+      ...transformRelationCardinality({
+        writer,
+        relation,
+        cardinality: rangeCardinality,
+        isDomain: false,
+      }),
+    ];
+  }
+
+  return [];
+}
+
+/**
+ * Transform relation cardinalities to gUFO
+ */
+export function transformRelationCardinality({
+  writer,
+  relation,
+  cardinality,
+  isDomain,
+}: {
+  writer: N3Writer;
+  relation: IRelation;
+  cardinality: string;
+  isDomain: boolean;
+}): Quad[] {
+  const { properties } = relation;
+  const domain = properties[isDomain ? 0 : 1];
+  const domainClassId = domain.propertyType.id;
+
+  const quads = [];
+
+  const lowerboundDomainCardinality = getLowerboundCardinality(cardinality);
+  const upperboundDomainCardinality = getUpperboundCardinality(cardinality);
+  const hasInfiniteCardinality = cardinality.includes('*');
+
+  // 1..* and *
+  if (
+    (lowerboundDomainCardinality === 1 ||
+      lowerboundDomainCardinality === upperboundDomainCardinality) &&
+    hasInfiniteCardinality
+  ) {
+    quads.push(
+      generateRelationCardinalityQuad({
+        writer,
+        relation,
+        isDomain,
+      }),
+    );
+  }
+  // min = max
+  else if (lowerboundDomainCardinality === upperboundDomainCardinality) {
+    quads.push(
+      generateRelationCardinalityQuad({
+        writer,
+        relation,
+        isDomain,
+        cardinalityPredicate: 'owl:qualifiedCardinality',
+        cardinality: lowerboundDomainCardinality,
+      }),
+    );
+  }
+  // 0..3, 0..5, ...
+  else if (lowerboundDomainCardinality === 0 && !hasInfiniteCardinality) {
+    quads.push(
+      generateRelationCardinalityQuad({
+        writer,
+        relation,
+        isDomain,
+        cardinalityPredicate: 'owl:maxQualifiedCardinality',
+        cardinality: upperboundDomainCardinality,
+      }),
+    );
+  }
+  // 2..*, 3..* ...
+  else if (lowerboundDomainCardinality > 1 && hasInfiniteCardinality) {
+    quads.push(
+      generateRelationCardinalityQuad({
+        writer,
+        relation,
+        isDomain,
+        cardinalityPredicate: 'owl:minQualifiedCardinality',
+        cardinality: lowerboundDomainCardinality,
+      }),
+    );
+  }
+  // 2..4, 3..7 ...
+  else if (lowerboundDomainCardinality > 1 && !hasInfiniteCardinality) {
+    quads.push(
+      quad(
+        namedNode(`:${domainClassId}`),
+        namedNode('rdfs:subClassOf'),
+        writer.blank([
+          {
+            predicate: namedNode('rdf:type'),
+            object: namedNode('owl:Class'),
+          },
+          {
+            predicate: namedNode('owl:intersectionOf'),
+            object: writer.list([
+              generateRelationBlankTriples({
+                writer,
+                relation,
+                isDomain,
+                cardinalityPredicate: 'owl:minQualifiedCardinality',
+                cardinality: lowerboundDomainCardinality,
+              }),
+              generateRelationBlankTriples({
+                writer,
+                relation,
+                isDomain,
+                cardinalityPredicate: 'owl:maxQualifiedCardinality',
+                cardinality: upperboundDomainCardinality,
+              }),
+            ]),
+          },
+        ]),
+      ),
+    );
+  }
+
+  return quads;
+}
+
+/**
+ * Transform relation cardinalities to gUFO
+ */
+export function generateRelationCardinalityQuad({
+  writer,
+  relation,
+  isDomain,
+  cardinalityPredicate,
+  cardinality,
+}: {
+  writer: N3Writer;
+  relation: IRelation;
+  isDomain: boolean;
+  cardinalityPredicate?: string;
+  cardinality?: number;
+}): Quad {
+  const { properties } = relation;
+  const domain = properties[isDomain ? 0 : 1];
+  const domainClassId = domain.propertyType.id;
+
+  return quad(
+    namedNode(`:${domainClassId}`),
+    namedNode('rdfs:subClassOf'),
+    generateRelationBlankTriples({
+      writer,
+      relation,
+      isDomain,
+      cardinalityPredicate,
+      cardinality,
+    }),
+  );
+}
+
+function generateRelationBlankTriples({
+  writer,
+  relation,
+  isDomain,
+  cardinalityPredicate,
+  cardinality,
+}: {
+  writer: N3Writer;
+  relation: IRelation;
+  isDomain: boolean;
+  cardinalityPredicate?: string;
+  cardinality?: number;
+}): BlankNode {
+  const { id, properties } = relation;
+  const range = properties[isDomain ? 1 : 0];
+  const rangeClassId = range.propertyType.id;
+
+  const blankTriples = [
+    {
+      predicate: namedNode('rdf:type'),
+      object: namedNode('owl:Restriction'),
+    },
+    {
+      predicate: namedNode('owl:onProperty'),
+      object: isDomain
+        ? namedNode(`:${id}`)
+        : writer.blank(namedNode('owl:inverseOf'), namedNode(`:${id}`)),
+    },
+  ];
+
+  if (cardinality && cardinalityPredicate) {
+    blankTriples.push({
+      predicate: namedNode(cardinalityPredicate),
+      object: literal(
+        cardinality.toString(),
+        namedNode('xsd:nonNegativeInteger'),
+      ),
+    });
+  }
+
+  blankTriples.push({
+    predicate: namedNode('owl:onClass'),
+    object: namedNode(`:${rangeClassId}`),
+  });
+
+  return writer.blank(blankTriples);
+}
+
+function getLowerboundCardinality(cardinality: string): number {
+  const cardinalities = cardinality.split('..');
+  const lowerbound = cardinalities[0];
+
+  return lowerbound === '*' ? 99999 : Number(lowerbound);
+}
+
+function getUpperboundCardinality(cardinality: string): number {
+  const cardinalities = cardinality.split('..');
+  const upperbound = cardinalities[1] || cardinalities[0];
+
+  return upperbound === '*' ? 99999 : Number(upperbound);
+}
