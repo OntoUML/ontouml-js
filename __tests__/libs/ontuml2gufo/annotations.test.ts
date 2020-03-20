3a3c01960df6f32d2c10829a5b782ce9eded65ff
Tiago Prince Sales <tiago.princesales@unibz.it>
2020-03-20 11:27:02 +0100

    Extended annotation tests
    A

--- __tests__/libs/ontuml2gufo/annotations.test.ts
+++ __tests__/libs/ontuml2gufo/annotations.test.ts
@@ -1,6 +1,6 @@
 import { ModelManager } from '@libs/model';
 import { OntoUML2GUFO } from '@libs/ontuml2gufo';
-import { istandard } from '@test-models/valids';
+import { annotations } from '@test-models/valids';
 import { IPackage, IOntoUML2GUFOOptions } from '@types';
 
 async function transformOntoUML2GUFO(
@@ -25,16 +25,45 @@ describe('Relations', () => {
   let result;
 
   beforeAll(async () => {
-    result = await transformOntoUML2GUFO(istandard);
+    result = await transformOntoUML2GUFO(annotations);
   });
 
-  it('should generate "nl" label', async () => {
-    expect(result).toContain('<:LegalGender> <rdfs:label> "Geslacht"@nl');
+  it('should generate language labels on classess (testing with nl, pt, en, and it)', async () => {
+    expect(result).toContain('<:Person> <rdfs:label> "Persoon"@nl');
+    expect(result).toContain('<:Person> <rdfs:label> "Pessoa"@pt');
+    expect(result).toContain('<:Person> <rdfs:label> "Person"@en');
+    expect(result).toContain('<:Person> <rdfs:label> "Persona"@it');
   });
 
-  it('should generate "description" as a rdfs:comment', async () => {
+  // it('should generate language labels on attributes (testing with nl, pt, en, and it)', async () => {
+  //   expect(result).toContain('<:name> <rdfs:label> "naam"@nl');
+  //   expect(result).toContain('<:name> <rdfs:label> "nome"@pt');
+  //   expect(result).toContain('<:name> <rdfs:label> "name"@en');
+  //   expect(result).toContain('<:name> <rdfs:label> "nome"@it');
+  // });
+
+  it('should generate language labels on classess (testing with nl, pt, en, and it)', async () => {
+    expect(result).toContain('<:RegardsEmployee> <rdfs:label> "betreft werknemer"@nl');
+    expect(result).toContain('<:RegardsEmployee> <rdfs:label> "de empregado"@pt');
+    expect(result).toContain('<:RegardsEmployee> <rdfs:label> "regards employee"@en');
+    expect(result).toContain('<:RegardsEmployee> <rdfs:label> "di dipendente"@it');
+  });
+
+  it('should generate class "description" as a rdfs:comment', async () => {
     expect(result).toContain(
-      '<:LegalGender> <rdfs:comment> "Gender of a person as registered in the civil registration."',
+      '<:Person> <rdfs:comment> "This is a description of the person class."',
+    );
+  });
+
+  // it('should generate attribute "description" as a rdfs:comment', async () => {
+  //   expect(result).toContain(
+  //     '<:name> <rdfs:comment> "This is the description of the attribute name in of the person class."',
+  //   );
+  // });
+
+  it('should generate association "description" as a rdfs:comment', async () => {
+    expect(result).toContain(
+      '<:RegardsEmployee> <rdfs:comment> "This is the description of the <<mediation>> association between the classes employment and person."',
     );
   });
 });
