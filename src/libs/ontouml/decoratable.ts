14a2ffaf747d1953dafb19399b7d7c449b395dec
Claudenir Fonseca <claudenirmf@gmail.com>
2021-02-02 07:38:53 +0100

    Update interface and utility files

--- src/libs/ontouml/decoratable.ts
+++ src/libs/ontouml/decoratable.ts
@@ -1,41 +1,5 @@
 import { ClassStereotype, RelationStereotype, PropertyStereotype } from './';
 
-function hasValidStereotypeValue<T extends ClassStereotype | RelationStereotype | PropertyStereotype>(
-  decoratable: Decoratable<T>,
-  validStereotypes: T[],
-  allowsNoStereotype: boolean = false
-): boolean {
-  try {
-    const uniqueStereotype = decoratable.stereotype;
-
-    return validStereotypes.includes(uniqueStereotype) || (!uniqueStereotype && allowsNoStereotype);
-  } catch (error) {
-    return false;
-  }
-}
-
-/** Checks if `this.stereotype` is contained in the set of values in `stereotypes`.
- *
- * @throws error when the class has multiple stereotypes
- * */
-function hasStereotypeContainedIn<Stereotype extends ClassStereotype | RelationStereotype | PropertyStereotype>(
-  decoratable: Decoratable<Stereotype>,
-  stereotypes: Stereotype | Stereotype[]
-): boolean {
-  const decoratableStereotype: Stereotype = decoratable.stereotype;
-
-  if (Array.isArray(stereotypes)) {
-    return stereotypes.includes(decoratableStereotype);
-  } else {
-    return decoratableStereotype === stereotypes;
-  }
-}
-
-export const decoratableUtils = {
-  hasValidStereotypeValue,
-  hasStereotypeContainedIn
-};
-
 export interface Decoratable<Stereotype extends ClassStereotype | RelationStereotype | PropertyStereotype> {
   stereotype: Stereotype;
 
