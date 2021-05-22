1edbe09e076d99b0d32c9fc501413647ca6603ce
Guylerme <guylerme@gmail.com>
2021-05-22 10:02:20 -0300

    Translation of comments and variables

--- src/libs/viewpoints/viewpoint_extractor.ts
+++ src/libs/viewpoints/viewpoint_extractor.ts
@@ -1,7 +1,7 @@
-import {Class, GeneralizationSet, Generalization, Diagram, Project, ClassStereotype, ModelElement, Relation} from '@libs/ontouml';
-import {Service} from '@libs/service';
-import {ServiceIssue} from '@libs/service_issue';
-import {Viewpoint} from './viewpoint';
+import { Class, GeneralizationSet, Generalization, Diagram, Project, ClassStereotype, ModelElement, Relation } from '@libs/ontouml';
+import { Service } from '@libs/service';
+import { ServiceIssue } from '@libs/service_issue';
+import { Viewpoint } from './viewpoint';
 
 /**
  * Class that implements the pattern-based model clustering strategy proposed in:
@@ -39,11 +39,18 @@ export class ViewpointExtractor implements Service {
   buildAll(): Diagram[] {
     const allPatterns = [ClassStereotype.KIND, ClassStereotype.SUBKIND, ClassStereotype.ROLE, ClassStereotype.PHASE, ClassStereotype.RELATOR, ClassStereotype.MODE, 'NON_SORTAL'];
     return allPatterns.map((pattern, index) => this.extractViewpoint(String(index), pattern))
-        .map((viewpoint) => viewpoint.createDiagram(this.project.model));
+      .map((viewpoint) => viewpoint.createDiagram(this.project.model));
   }
 
-  extractViewpoint(id:string, pattern: string):Viewpoint {
-    let viewpoint = new Viewpoint('Viewpoint of '+ pattern);
+  /**
+   * This methor will run the patter viewpoint extrator.
+   * 
+   * @param id The name of viewpoint extractor 
+   * @param pattern The viewpoint extractor approach based on OntoUML Patterns
+   * @returns A viewpoint based on a pattern choosen
+   */
+  extractViewpoint(id: string, pattern: string): Viewpoint {
+    let viewpoint = new Viewpoint('Viewpoint of ' + pattern);
 
     switch (pattern) {
       case ClassStereotype.KIND: viewpoint = this.kindView();
@@ -66,8 +73,20 @@ export class ViewpointExtractor implements Service {
     return viewpoint;
   }
 
-  kindView() : Viewpoint {
+  /**
+   * This method filter all Kinds and related relations ando group them in a view
+   * 
+   * @returns A viewpoint extracted using the Kind Pattern
+   */
+  kindView(): Viewpoint {
     const viewpoint = new Viewpoint('Kind View');
+
+    /**Step 1 Get all Kinds in model
+     * 
+     * Step 2 Get all relations between Kinds
+     * 
+     * Step 3 Get all generalizations between Kinds
+     */
 
     const classes = this.project.getAllClassesByStereotype(ClassStereotype.KIND);
 
@@ -80,8 +99,22 @@ export class ViewpointExtractor implements Service {
     return viewpoint;
   }
 
-  subKindView() :Viewpoint {
+  /**
+     * This method filter all Subkinds, respective sortal ancestors  and related relations and group them in a view
+     * 
+     * @returns A viewpoint extracted using the Subkind Pattern
+     */
+  subKindView(): Viewpoint {
     const viewpoint = new Viewpoint('SubKind View');
+
+    /**Step 1 Get all Subkinds in model
+     * 
+     * Step 2 Get all sortal ancestors 
+     * 
+     * Step 3 Get all relations between all classes (subkinds and sortal ancestors)
+     * 
+     * Step 4 Get all generalizations between all classes (subkinds and sortal ancestors)
+     */
 
     const classes = this.project.getAllClassesByStereotype(ClassStereotype.SUBKIND);
 
@@ -103,8 +136,23 @@ export class ViewpointExtractor implements Service {
     return viewpoint;
   }
 
-  roleView() :Viewpoint {
+
+  /**
+     * This method filter all Roles, respective sortal ancestors  and related relations and group them in a view
+     * 
+     * @returns A viewpoint extracted using the Role Pattern
+     */
+  roleView(): Viewpoint {
     const viewpoint = new Viewpoint('Role View');
+
+    /**Step 1 Get all Roles in model
+     * 
+     * Step 2 Get all sortal ancestors 
+     * 
+     * Step 3 Get all relations between all classes (roles and sortal ancestors)
+     * 
+     * Step 4 Get all generalizations between all classes (roles and sortal ancestors)
+     */
 
     const classes = this.project.getAllClassesByStereotype(ClassStereotype.ROLE);
 
@@ -126,9 +174,23 @@ export class ViewpointExtractor implements Service {
     return viewpoint;
   }
 
-  phaseView() :Viewpoint {
+  /**
+     * This method filter all Phases, respective sortal ancestors  and related relations and group them in a view
+     * 
+     * @returns A viewpoint extracted using the Phase Pattern
+     */
+  phaseView(): Viewpoint {
     const viewpoint = new Viewpoint('Phase View');
 
+
+    /**Step 1 Get all Phases in model
+     * 
+     * Step 2 Get all sortal ancestors 
+     * 
+     * Step 3 Get all relations between all classes (phases and sortal ancestors)
+     * 
+     * Step 4 Get all generalizations between all classes (phases and sortal ancestors)
+     */
     const classes = this.project.getAllClassesByStereotype(ClassStereotype.PHASE);
 
     let generals = [];
@@ -149,9 +211,26 @@ export class ViewpointExtractor implements Service {
     return viewpoint;
   }
 
+  /**
+     * This method filter all Relators, respective direct conectect clases by mediator relation, their sortal ancestors  
+     * and related relations and group them in a view
+     * 
+     * @returns A viewpoint extracted using the Relator Pattern
+     */
   relatorView = () => {
     const viewpoint = new Viewpoint('Relator View');
 
+
+    /**Step 1 Get all Relators in model
+     * 
+     * Step 2 Get all classes conected by mediation relation 
+     * 
+     * Step 3 Get all sortal ancestors 
+     * 
+     * Step 4 Get all relations between all classes 
+     * 
+     * Step 5 Get all generalizations between all classes 
+     */
     let classes = this.project.getAllClassesByStereotype(ClassStereotype.RELATOR);
 
     const relacoes = this.project.getAllRelations();
@@ -188,9 +267,24 @@ export class ViewpointExtractor implements Service {
     return viewpoint;
   }
 
-  nonSortalView() :Viewpoint {
+  /**
+     * This method filter all Non-Sortals, their descendants, then the decendants sortal ancestors  and related 
+     * relations and group them in a view
+     * 
+     * @returns A viewpoint extracted using the Non-Sortal Pattern
+     */
+  nonSortalView(): Viewpoint {
     const viewpoint = new Viewpoint('Non Sortal View');
 
+    /**Step 1 Get all Non-Sortals in model
+     * 
+     * Step 2 Get all non-sortals decendants 
+     * 
+     * Step 3 Get all decendants sortal ancestors 
+     * 
+     * Step 4 Get all relations between all classes 
+     * 
+     */
     let classes = this.project.getAllClassesByStereotype(ClassStereotype.ROLE_MIXIN);
 
     classes = classes.concat(this.project.getAllClassesByStereotype(ClassStereotype.MIXIN));
@@ -223,9 +317,23 @@ export class ViewpointExtractor implements Service {
     return viewpoint;
   }
 
-  modeView() :Viewpoint {
+  /**
+     * This method filter all Modes, their descendants, then the decendants sortal ancestors  and related relations and group them in a view
+     * 
+     * @returns A viewpoint extracted using the Mode Pattern
+     */
+  modeView(): Viewpoint {
     const viewpoint = new Viewpoint('Mode View');
 
+    /**Step 1 Get all Modes in model
+      * 
+      * Step 2 Get all modes decendants 
+      * 
+      * Step 3 Get all decendants sortal ancestors 
+      * 
+      * Step 4 Get all relations between all classes
+      * 
+      */
     let classes = this.project.getAllClassesByStereotype(ClassStereotype.MODE);
 
     let decendentes = [];
@@ -256,25 +364,25 @@ export class ViewpointExtractor implements Service {
   }
 
   /**
-   * Definicao da funcao que ira checar se uma determinada classe participa da relacoa
-   * @param classe A classe a ser verificada na relacao
-   * @param relacao A relacao que pode possuir a classe passada
+   * Verify is a class participates in relation
+   * @param _class The classe to be verified
+   * @param relation The relation where the class may be participating
    */
-  isClassInRelation(classe:Class, relacao:Relation) : boolean {
-    if (classe == relacao.getSourceClass() || classe == relacao.getTargetClass()) {
+  isClassInRelation(_class: Class, relation: Relation): boolean {
+    if (_class == relation.getSourceClass() || _class == relation.getTargetClass()) {
       return true;
     } else return false;
   }
 
   /**
-   * Definicao da funcao que ira verificar se no origem de um determinado relacioanamento possui alguma classe da lista passada
+   * This method will verify if any class is in Source of relation
    *
-   * @param classes Lista de classes para verificar se alguma delas está no Origem da relacao
-   * @param relacao Relacao que ser a verificada se no seu origem contem alguma das classes passada
+   * @param classes List of classes to be verified if any of them is in the Source of relation
+   * @param relation The relation where the class may be participating
    */
-  hasClassInSourceOfRelation(classes : Class[], relacao : Relation) : boolean {
+  hasClassInSourceOfRelation(classes: Class[], relation: Relation): boolean {
     for (let i = 0; i < classes.length; i++) {
-      if (classes[i] == relacao.getSourceClass()) {
+      if (classes[i] == relation.getSourceClass()) {
         return true;
       }
     }
@@ -282,14 +390,14 @@ export class ViewpointExtractor implements Service {
   }
 
   /**
-   * Definicao da funcao que ira verificar se no destino de um determinado relacioanamento possui alguma classe da lista passada
+   * This method will verify if any class is in Target of relation
    *
-   * @param classes Lista de classes para verificar se alguma delas está no Destino da relacao
-   * @param relacao Relacao que ser a verificada se no seu destino contem alguma das classes passada
+   * @param classes List of classes to be verified if any of them is in the Target of relation
+   * @param relation The relation where the class may be participating
    */
-  hasClassInTargetOfRelation(classes : Class[], relacao : Relation) : boolean {
+  hasClassInTargetOfRelation(classes: Class[], relation: Relation): boolean {
     for (let i = 0; i < classes.length; i++) {
-      if (classes[i] == relacao.getTargetClass()) {
+      if (classes[i] == relation.getTargetClass()) {
         return true;
       }
     }
@@ -297,14 +405,13 @@ export class ViewpointExtractor implements Service {
   }
 
   /**
-   * Definicao da funcao que ira verificar se um determinado relacionamento tem as suas partes (origem e destino) na lista de classes passadas
-   * Esta funcao e utilizada para recuperar as relacoes existentes entre elementos contidos na lista de classes
+   * This method will verify if a relation has in its parts (source and target) any of class in the list
    *
-   * @param classes Lista de classes para verificar quais relações existem entre elas
-   * @param relacao A relacao que ira verificar se na origem e no destino contem classes da lista passada
+   * @param classes List of classes to be verified if any of them is in the Target of relation
+   * @param relation The relation where the class may be participating
    */
-  relationWithSourceAndTarget(classes : Class[], relacao : Relation) : boolean {
-    if (this.hasClassInSourceOfRelation(classes, relacao) && (this.hasClassInTargetOfRelation(classes, relacao))) {
+  relationWithSourceAndTarget(classes: Class[], relation: Relation): boolean {
+    if (this.hasClassInSourceOfRelation(classes, relation) && (this.hasClassInTargetOfRelation(classes, relation))) {
       return true;
     } else {
       return false;
@@ -312,33 +419,33 @@ export class ViewpointExtractor implements Service {
   }
 
   /**
- * Definicao da funcao que obter a lista de relacoes entre as classes passadas
- *
- * @param classes Lista de classes utilizada para obter as relacoes entre elas
- */
-  getRelationsBetweenClasses(classes: Class[]) : Relation[] {
-    const relacoes = this.project.getAllRelations();
-
-    let relacoesRetorno = [];
-
-    for (let i = 0; i < relacoes.length; i++) {
-      if (this.relationWithSourceAndTarget(classes, relacoes[i])) {
-        relacoesRetorno = relacoesRetorno.concat(relacoes[i]);
-      }
-    }
-
-    return relacoesRetorno;
-  }
-
-  /**
-   * Definicao da funcao que ira verificar se no general de uma determinada generalizacao possui alguma classe da lista passada
+   * This method get the relations between the classes
    *
-   * @param classes Lista de classes para verificar se alguma delas está no General da generelizacao
-   * @param generalizacao Generalizacao que ser a verificada se no seu general contem alguma das classes passada
+   * @param classes List of classes
    */
-  hasClassInGeneralOfGeneralization(classes:Class[], generalizacao:Generalization) : boolean {
+  getRelationsBetweenClasses(classes: Class[]): Relation[] {
+    const relations = this.project.getAllRelations();
+
+    let returnRelations = [];
+
+    for (let i = 0; i < relations.length; i++) {
+      if (this.relationWithSourceAndTarget(classes, relations[i])) {
+        returnRelations = returnRelations.concat(relations[i]);
+      }
+    }
+
+    return returnRelations;
+  }
+
+  /**
+   * This method will verify if any class in the list is the general of generalization
+   *
+   * @param classes List of classes to be verified
+   * @param generalizacao Generalization to check if any class is its general
+   */
+  hasClassInGeneralOfGeneralization(classes: Class[], generalization: Generalization): boolean {
     for (let i = 0; i < classes.length; i++) {
-      if (classes[i] == generalizacao.general) {
+      if (classes[i] == generalization.general) {
         return true;
       }
     }
@@ -346,14 +453,14 @@ export class ViewpointExtractor implements Service {
   }
 
   /**
- * Definicao da funcao que ira verificar se no specific de uma determinada generalizacao possui alguma classe da lista passada
- *
- * @param classes Lista de classes para verificar se alguma delas está no Specific da generelizacao
- * @param generalizacao Generalizacao que ser a verificada se no seu specific contem alguma das classes passada
- */
-  hasClassInSpecificOfGeneralization(classes:Class[], generalizacao:Generalization) : boolean {
+   * This method will verify is any class in the list is the specific of generalization
+   *
+   * @param classes List of classes to be verified
+   * @param generalizacao Generalization to check if any class is its specific
+   */
+  hasClassInSpecificOfGeneralization(classes: Class[], generalization: Generalization): boolean {
     for (let i = 0; i < classes.length; i++) {
-      if (classes[i] == generalizacao.specific) {
+      if (classes[i] == generalization.specific) {
         return true;
       }
     }
@@ -361,14 +468,13 @@ export class ViewpointExtractor implements Service {
   }
 
   /**
- * Definicao da funcao que ira verificar se uma determinada generalizacao tem as suas partes (general e specific) na lista de classes passadas
- * Esta funcao e utilizada para recuperar as generalizacoes existentes entre elementos contidos na lista de classes
- *
- * @param classes Lista de classes para verificar quais generalizacoes existem entre elas
- * @param generalizacao A generalizacao que ira verificar se no pai e no filho contem classes da lista passada
- */
-  generalizationWithGeneralAndSpecific(classes:Class[], generalizacao:Generalization) :boolean {
-    if (this.hasClassInGeneralOfGeneralization(classes, generalizacao) && (this.hasClassInSpecificOfGeneralization(classes, generalizacao))) {
+   * This method will verify is any class in the list is the general or the specific of generalization
+   *
+   * @param classes List of classes to be verified
+   * @param generalizacao Generalization to check if any class is its specific or its general
+   */
+  generalizationWithGeneralAndSpecific(classes: Class[], generalization: Generalization): boolean {
+    if (this.hasClassInGeneralOfGeneralization(classes, generalization) && (this.hasClassInSpecificOfGeneralization(classes, generalization))) {
       return true;
     } else {
       return false;
@@ -376,29 +482,38 @@ export class ViewpointExtractor implements Service {
   }
 
   /**
-* Definicao da funcao que obter a lista de generalizacoes entre as classes passadas
-*
-* @param classes Lista de classes utilizada para obter as generalizacoes entre elas
-*/
-  getGeneralizationsBetweenClasses(classes:Class[]) : Generalization[] {
-    const generalizacoes = this.project.getAllGeneralizations();
+    * This method gets the list of generalizations between classes
+    *
+    * @param classes List of classes to obtain the generalizations between then
+    */
+  getGeneralizationsBetweenClasses(classes: Class[]): Generalization[] {
+    const generalizations = this.project.getAllGeneralizations();
 
-    let generalizacoesRetorno = [];
+    let returnGeneralizations = [];
 
-    for (let i = 0; i < generalizacoes.length; i++) {
-      if (this.generalizationWithGeneralAndSpecific(classes, generalizacoes[i])) {
-        generalizacoesRetorno = generalizacoesRetorno.concat(generalizacoes[i]);
+    for (let i = 0; i < generalizations.length; i++) {
+      if (this.generalizationWithGeneralAndSpecific(classes, generalizations[i])) {
+        returnGeneralizations = returnGeneralizations.concat(generalizations[i]);
       }
     }
 
-    return generalizacoesRetorno;
+    return returnGeneralizations;
   }
 
-
+  /**
+    * This method gets the classes in specific line of a NonSortal
+    *
+    * @param classes The non-sortal to get its specifics
+    */
   static getNonSortalLine(nonSortal: Class): Class[] {
     return this.traverseNonSortalLine(nonSortal, []);
   }
 
+  /**
+    * This method gets the classes in specific line of a NonSortal
+    *
+    * @param classes The non-sortal to get its specifics
+    */
   private static traverseNonSortalLine(nonSortal: Class, path: Class[]): Class[] {
     nonSortal.getChildren().forEach((child) => {
       if (!(child instanceof Class) || !child.stereotype) return;
@@ -411,6 +526,7 @@ export class ViewpointExtractor implements Service {
     return path;
   }
 
+  
   getGeneralizationSetsFrom(referenceGens: Generalization[]): GeneralizationSet[] {
     const gsInvolvesAll = this.project.getGeneralizationSetsInvolvingAll(referenceGens);
     const partitionInvolvesAny = this.project.getGeneralizationSetsInvolvingAny(referenceGens).filter((gs) => gs.isPhasePartition());
