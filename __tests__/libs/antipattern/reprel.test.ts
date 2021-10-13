f2a7d75bbe4f4ea8950d90dc3e348ad43df6ec86
Mattia Fumagalli <mattia.fumagalli@gmail.com>
2021-10-13 13:54:11 +0200

    RepRel anti-pattern finder
    WIP

--- __tests__/libs/antipattern/reprel.test.ts
+++ __tests__/libs/antipattern/reprel.test.ts
@@ -3,41 +3,97 @@ import { RepRelOccurrence } from '@libs/antipattern/reprel_occurrence';
 import { Project } from '@libs/ontouml';
 
 describe('RepRel Test 0', () => {
-    const project = new Project();
-    const model = project.createModel();
-  
-    const person = model.createKind('Person');
-    const supervisor = model.createRole('Supervisor');
-    const student = model.createRole('Student');
-    const university = model.createKind('University'); //this generates the issue
-    const supervision = model.createRelator('Supervision');
-  
-    const gen1 = model.createGeneralization(person, student);
-    const gen2 = model.createGeneralization(person, supervisor);
-  
-    const med1 = model.createMediationRelation(supervision, supervisor);
-    med1.getSourceEnd().cardinality.setOneToMany();
-  
-    const med2 = model.createMediationRelation(supervision, student);
-    med2.getSourceEnd().cardinality.setOneToMany();
-  
-    const med3 = model.createMediationRelation(supervision, university); //this generates the issue
-    med3.getSourceEnd().cardinality.setOneToMany(); //this generates the issue
-  
-    const repRel = new RepRelFinder(project);
-    const output = repRel.run();
-  
-    it('should find the RepRel occurrence', () => {
-      expect(output.result).toHaveLength(1);
-    });
-  
-    it('should set «relator» Supervision in the RepRel occurrence', () => {
-        const reprel: RepRelOccurrence = output.result[0];
-        expect(reprel.relator).toEqual(supervision);
-    });
+  const project = new Project();
+  const model = project.createModel();
+
+  const person = model.createKind('Person');
+  const supervisor = model.createRole('Supervisor');
+  const student = model.createRole('Student');
+  const university = model.createKind('University'); //this generates the issue
+  const supervision = model.createRelator('Supervision');
+
+  const gen1 = model.createGeneralization(person, student);
+  const gen2 = model.createGeneralization(person, supervisor);
+
+  const med1 = model.createMediationRelation(supervision, supervisor);
+  med1.getSourceEnd().cardinality.setOneToMany();
+
+  const med2 = model.createMediationRelation(supervision, student);
+  med2.getSourceEnd().cardinality.setOneToMany();
+
+  const med3 = model.createMediationRelation(supervision, university);
+  med3.getSourceEnd().cardinality.setOneToMany();
+
+  const repRel = new RepRelFinder(project);
+  const output = repRel.run();
+
+  it('should find the RepRel occurrence', () => {
+    expect(output.result).toHaveLength(1);
   });
 
-// it('should not find any relator occurrence', () => {
-//     const relover: RepRelOccurrence = output.result[0]; 
-//     expect(relover).toBeUndefined();
-// });
+  it('should set «relator» Supervision in the RepRel occurrence', () => {
+    const reprel: RepRelOccurrence = output.result[0];
+    expect(reprel.relator).toEqual(supervision);
+  });
+});
+
+describe('RepRel Test 1', () => {
+  const project = new Project();
+  const model = project.createModel();
+
+  const person = model.createKind('Person');
+  const supervisor = model.createRole('Supervisor');
+  const student = model.createRole('Student');
+  const university = model.createKind('University'); //this generates the issue
+  const supervision = model.createRelator('Supervision');
+
+  const gen1 = model.createGeneralization(person, student);
+  const gen2 = model.createGeneralization(person, supervisor);
+
+  const med1 = model.createMediationRelation(supervision, supervisor);
+  med1.getSourceEnd().cardinality.setOneToMany();
+
+  const repRel = new RepRelFinder(project);
+  const output = repRel.run();
+
+  it('should find the RepRel occurrence', () => {
+    expect(output.result).toHaveLength(0);
+  });
+
+  it('should set «relator» Undefined in the RepRel occurrence', () => {
+    const reprel: RepRelOccurrence = output.result[0];
+    expect(reprel).toBeUndefined();
+  });
+});
+
+describe('RepRel Test 2', () => {
+  const project = new Project();
+  const model = project.createModel();
+
+  const person = model.createKind('Person');
+  const supervisor = model.createRole('Supervisor');
+  const student = model.createRole('Student');
+  const university = model.createKind('University'); //this generates the issue
+  const supervision = model.createRelator('Supervision');
+
+  const gen1 = model.createGeneralization(person, student);
+  const gen2 = model.createGeneralization(person, supervisor);
+
+  const med1 = model.createMediationRelation(supervision, supervisor);
+  med1.getSourceEnd().cardinality.setUpperBoundFromNumber(1);
+
+  const med3 = model.createMediationRelation(supervision, university);
+  med3.getSourceEnd().cardinality.setOneToMany();
+
+  const repRel = new RepRelFinder(project);
+  const output = repRel.run();
+
+  it('should find the RepRel occurrence', () => {
+    expect(output.result).toHaveLength(0);
+  });
+
+  it('should set «relator» Undefined in the RepRel occurrence', () => {
+    const reprel: RepRelOccurrence = output.result[0];
+    expect(reprel).toBeUndefined();
+  });
+});
--- src/libs/antipattern/reprel_finder.ts
+++ src/libs/antipattern/reprel_finder.ts
@@ -11,45 +11,41 @@ import _, { noConflict } from 'lodash';
  *
  * @author Tiago Sales
  * @author Mattia Fumagalli
- * 
+ *
  */
 
 export class RepRelFinder implements Service {
-    project: Project;
+  project: Project;
 
-    constructor(project: Project) {
-        this.project = project;
+  constructor(project: Project) {
+    this.project = project;
+  }
+
+  run(): { result: RepRelOccurrence[]; issues?: ServiceIssue[] } {
+    const relators = this.project.getClassesWithRelatorStereotype();
+    const antiPatternOccurrences: RepRelOccurrence[] = [];
+
+    for (const relator of relators) {
+      const FoundOccurrences = this.findOccurrence(relator);
+      if (!_.isEmpty(FoundOccurrences)) antiPatternOccurrences.push(FoundOccurrences);
     }
+    return { result: antiPatternOccurrences };
+  }
 
-    run(): { result: RepRelOccurrence[]; issues?: ServiceIssue[] } {
-        const relators = this.project.getClassesWithRelatorStereotype();
-        const AntiPatternOccurrences: RepRelOccurrence[] = [];
+  findOccurrence(relator: Class): RepRelOccurrence {
+    const mediations = this.project
+      .getAllRelationsByStereotype(RelationStereotype.MEDIATION)
+      .filter(mediation => mediation.getSourceClass() === relator);
 
-        for (const relator of relators) {
-            const FoundOccurrences = this.findOccurrence(relator);
-            if (!_.isEmpty(FoundOccurrences)) AntiPatternOccurrences.push(FoundOccurrences);
-        }
-        return { result: AntiPatternOccurrences };
+    const mediationProperties = mediations.map(mediation => mediation.getSourceEnd());
+    const sourceCardinalities = mediationProperties.map(sourceCardinality => sourceCardinality.cardinality);
+    const sourceCardinalitiesUpperBound = sourceCardinalities.map(upperBound => upperBound.getUpperBoundAsNumber());
+
+    if (mediations && mediations.length >= 2) {
+      const cardinalitiesCheck = sourceCardinalitiesUpperBound.filter(x => x > 1);
+      if (cardinalitiesCheck && cardinalitiesCheck.length >= 2) return new RepRelOccurrence(relator, mediations);
+      console.log(`cardinalities check: `, cardinalitiesCheck);
     }
-
-    findOccurrence(relator: Class): RepRelOccurrence {
-        const mediations = this.project
-            .getAllRelationsByStereotype(RelationStereotype.MEDIATION)
-            .filter(mediation => mediation.getSourceClass() === relator);
-
-        const mediationProperties = mediations.map(mediation => mediation.getSourceEnd());
-        const sourceCardinalities = mediationProperties.map(sourceCardinality => sourceCardinality.cardinality);
-        const sourceCardinalitiesUpperBound = sourceCardinalities.map(upperBound => upperBound.getUpperBoundAsNumber());
-        console.log(`checking sourceCardinalitiesUpperBound: `, sourceCardinalitiesUpperBound);
-
-        if (mediations && mediations.length >= 2) {
-            console.log(`mediations lenght: `, mediations.length);
-            const mediationSourceCardinalities = sourceCardinalitiesUpperBound;
-            const CardinalitiesCheck = mediationSourceCardinalities.filter(x => x > 0);
-            console.log(`cardinalities check: `, CardinalitiesCheck);
-            if (CardinalitiesCheck && CardinalitiesCheck.length > 2) return new RepRelOccurrence(relator, mediations);
-        }
-        return null
-    }
+    return null;
+  }
 }
-
