57dedc5a8ceac3fa1b036c1432c18c9cc231ad77
Asen <a.pantov@student.utwente.nl>
2023-04-05 14:19:48 +0200

    class_functions fixing, added helper functions to helper.ts

---
+++ __tests__/libs/ontouml2alloy/generalization_functions.test.ts
@@ -0,0 +1,6 @@
+import { Generalization } from '@libs/ontouml';
+import { Ontouml2Alloy } from '@libs/ontouml2alloy/index';
+import { getNameNoSpaces } from '@libs/ontouml2alloy/util';
+import { generateAlloy } from '@libs/ontouml2alloy/helper';
+import { Package } from '@libs/ontouml';
+
