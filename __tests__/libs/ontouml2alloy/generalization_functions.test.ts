8c877c92dbf46a6c4ccdb4eaa45278d8527da0ed
Asen <a.pantov@student.utwente.nl>
2023-05-04 01:03:54 +0200

    generalization & gen_set & helpers update

--- __tests__/libs/ontouml2alloy/generalization_functions.test.ts
+++ __tests__/libs/ontouml2alloy/generalization_functions.test.ts
@@ -1,12 +1,11 @@
 import { Generalization } from '@libs/ontouml';
 import { Ontouml2Alloy } from '@libs/ontouml2alloy/index';
 import { normalizeName } from '@libs/ontouml2alloy/util';
-import { generateAlloy, generateFact, generateWorldAttribute } from './helpers';
+import { generateAlloy, generateFact, generateWorldAttribute, generateFun, generateWorldFact } from './helpers';
 import { Package, Project } from '@libs/ontouml';
 
 describe('Generalization functions', () => {
 
-  
     describe ('transformGeneralization function',() => {
 
         let project: Project;
@@ -19,7 +18,6 @@ describe('Generalization functions', () => {
 
 
         it('Between classes', () => {
-
             const parent = model.createKind('Person');
             const child = model.createSubkind('Man');
             model.createGeneralization(parent, child);
@@ -31,12 +29,11 @@ describe('Generalization functions', () => {
             expect(result).toContain(generateFact('generalization',['Man in Person']));
 
             expect(result).toContain(generateFact('rigid',['rigidity[Man,Object,exists]']))
-            expect(result).toContain(generateWorldAttribute('Man','Object')); //expected? -Yes
-
+            expect(result).toContain(generateWorldAttribute('Man','Object'));
           });
         
+          //TODO discuss again if there should be such a test/handling such a situation
           it('Between classes without stereotypes', () => {
-            const model = new Package();
             const parent = model.createClass('Person');
             const child = model.createClass('Man');
             model.createGeneralization(parent, child);
@@ -46,26 +43,49 @@ describe('Generalization functions', () => {
             //what is expected?; should it be allowed to have such a model? - NO
           });
         
+          it('Between datatypes', () => {
+            const parent = model.createDatatype('Color');
+            const child1 = model.createDatatype('ColorInRgb');
+            model.createGeneralization(parent, child1);
+
+            let result = generateAlloy(model);
+
+            expect(result).toContain(generateFact('generalization',['ColorInRgb in Color']));
+            expect(result).toContain('sig Color in Datatype {}');
+            expect(result).toContain('sig ColorInRgb in Datatype {}');
+            expect(result).toContain(generateFact('additionalDatatypeFacts',['Datatype = Color+ColorInRgb']))
+
+          });
+
           it('Between relations', () => {
-            const model = new Package();
             const _class = model.createKind('Person');
             const parent = model.createMaterialRelation(_class, _class, 'likes');
             const child = model.createMaterialRelation(_class, _class, 'loves');
             model.createGeneralization(parent, child);
         
             const result = generateAlloy(model);
-            console.log(result); 
+            expect(result).toContain(generateFact('rigid',['rigidity[Person,Object,exists]']));
+            expect(result).toContain(generateWorldAttribute('Person','Object'));
+            expect(result).toContain(generateWorldFact('Person','Object'));
+
+            expect(result).toContain(generateFact('generalization',['loves in likes']));
+          
+            expect(result).toContain(generateFun('Person1', 'Person', `(w.likes).x`));
+            expect(result).toContain(generateFun('Person2', 'Person', `x.(w.likes)`));
+            expect(result).toContain(generateFun('Person3', 'Person', `(w.loves).x`));
+            expect(result).toContain(generateFun('Person4', 'Person', `x.(w.loves)`));
+
           });
         
+          //TODO check if this is desired behaviour; should there be no difference with previous casee?
           it('Between relations without stereotypes', () => {
-            const model = new Package();
             const _class = model.createKind('Person');
             const parent = model.createBinaryRelation(_class, _class, 'likes');
             const child = model.createBinaryRelation(_class, _class, 'loves');
             model.createGeneralization(parent, child);
         
-            const owl = generateAlloy(model);
-            console.log(owl);
+            const result = generateAlloy(model);
+            console.log(result);
           });
 
 
